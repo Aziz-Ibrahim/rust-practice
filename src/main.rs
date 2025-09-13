fb1d31322ba014a59abb5dc6f21562a64f12b16f
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-09-13 18:52:57 +0100

    practicing IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x = 8;
     let y = 2;
@@ -9,4 +11,11 @@ fn main() {
     const HOURS_IN_A_DAY: u32 = 24;
     const SECONDS_IN_A_DAY: u32 = SECONDS_IN_A_MINUTE * MINUTES_IN_AN_HOUR * HOURS_IN_A_DAY;
     println!("There are {} seconds in a day.", SECONDS_IN_A_DAY);
+
+    let mut input = String::new();
+    println!("Enter a number:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let num: i32 = input.trim().parse().expect("Please enter a valid number");
+    let squared = num * num;
+    println!("The square of {} is {}", num, squared);
 }
